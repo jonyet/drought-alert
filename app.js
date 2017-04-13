0839c97514d6376744c1e77635f6db95698cf6c8
Jonathan Terry <jterry@hugeinc.com>
2017-04-12 22:09:01 -0400

    dumb varname removal; introduced moment to handle dating

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 import request from 'request';
+import moment from 'moment';
 import dotenv from 'dotenv';
 dotenv.config();
 
 // example api call = http://api.wunderground.com/api/cdc9167d053abdfa/history_YYYYMMDD/q/CA/San_Francisco.json
-
 // const url = 'http://api.wunderground.com/api/cdc9167d053abdfa/history_20170331/q/GA/Marietta.json'
 
-const date = '20170331';
+const date = moment().subtract(1, 'day').format('YYYYMMDD'); //'20170331';
 const state = 'GA';
-const city_name = 'Marietta'
-const url = `http://api.wunderground.com/api/${process.env.API_KEY}/history_${date}/q/${state}/${city_name}.json`;
+const city = 'Marietta'
+const url = `http://api.wunderground.com/api/${process.env.API_KEY}/history_${date}/q/${state}/${city}.json`;
 
 console.log(url);
 
