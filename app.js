3bf648d55df2dbe37c1d35a5b72eda6d312d4f27
Jonathan Terry <jterry@hugeinc.com>
2017-04-14 15:29:43 -0400

    isolated key data for the daily fetch

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+import rp from 'request-promise';
 import request from 'request';
 import moment from 'moment';
 import dotenv from 'dotenv';
@@ -11,9 +12,12 @@ const state = 'GA';
 const city = 'Marietta'
 const url = `http://api.wunderground.com/api/${process.env.API_KEY}/history_${date}/q/${state}/${city}.json`;
 
-console.log(url);
-
-request(url, (err, res) => {
-	console.log(res.statusCode, res.statusMessage);
-	console.log(res.body);
-});
+rp(url)
+	.then((res) => {
+		const data = JSON.parse(res)
+		const precipi = data.history.dailysummary[0].precipi
+		console.log(precipi);
+		(precipi < 0.33) ? (console.log('water shit')) : (console.log('you good dog'))
+		return res
+	})
+	.catch((err) => console.error(err))
