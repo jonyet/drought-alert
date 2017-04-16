c6da7f40e36f415adeebbaf840b93da787c4b3c3
Jonathan Terry <jterry@hugeinc.com>
2017-04-16 00:20:24 -0400

    made date range better, put logging back in, fixed usage of parseInt to be parseFloat so the math is right

--- app.js
+++ app.js
@@ -7,9 +7,9 @@ const location = {
 };
 
 const threshold = [
-	moment().subtract(1, 'day').format('YYYYMMDD'),
-	moment().subtract(2, 'day').format('YYYYMMDD'),
-	moment().subtract(3, 'day').format('YYYYMMDD')
+	moment().subtract(11, 'day').format('YYYYMMDD'),
+	moment().subtract(12, 'day').format('YYYYMMDD'),
+	moment().subtract(13, 'day').format('YYYYMMDD')
 ]
 
 const date = moment().subtract(1, 'day').format('YYYYMMDD'); //'20170331';
