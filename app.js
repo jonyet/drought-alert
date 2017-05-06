efb28470cd3c9a70fa3f6a151ba573a3a9dfd2a8
Jonathan Terry <jterry@hugeinc.com>
2017-05-06 15:26:39 -0400

    completed async data return ops and made a stub for the alert

--- app.js
+++ app.js
@@ -6,13 +6,22 @@ const location = {
 				city: 'Marietta'
 };
 
+const alertTrueData = ["20170405", "20170404", "20170403"];
+const alertFalseData = ["20170414", "20170413", "20170412"];
+
 const threshold = [
-	moment().subtract(11, 'day').format('YYYYMMDD'),
-	moment().subtract(12, 'day').format('YYYYMMDD'),
-	moment().subtract(13, 'day').format('YYYYMMDD')
-]
+	moment().subtract(1, 'day').format('YYYYMMDD'),
+	moment().subtract(2, 'day').format('YYYYMMDD'),
+	moment().subtract(3, 'day').format('YYYYMMDD')
+];
 
-const date = moment().subtract(1, 'day').format('YYYYMMDD'); //'20170331';
+let rainfall = Weather.fetchPrecipSum(threshold, location);
 
-// Weather.fetchRainPast(date, location);
-Weather.fetchPrecipSum(threshold, location);
+rainfall.then(function(result) {
+   console.log(`${result} inches of rainfall in the last three days`);
+	 if (result < 0.5) {
+		 console.log('alert user to water');
+	 } else {
+		 console.log('do not throw an alert');
+	 }
+});
