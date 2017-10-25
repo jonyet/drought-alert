23a0f9f2e664e96c4f1cca5f1c3dd5817ba7c21f
O'Dwayne Irving <oirving@hugeinc.com>
2017-10-25 16:21:41 -0400

    Add slack integration

--- test/alert.test.js
+++ test/alert.test.js
@@ -1,36 +1,36 @@
 import rp from 'request-promise';
-import { expect } from 'chai';
+import {expect} from 'chai';
 
 describe('Alerts:', () => {
   describe('alerts sent to slack', () => {
     it('should authenticate successfully', () => {
       //expect 200 OK
     });
-
+    
     it('should know when a channel is the target', () => {
       //expect slackChannel() to be called
     });
-
+    
     it('should know when a user is the target', () => {
       //expect slackDirect() to be called
     });
   });
-
+  
   describe('alerts sent straight to a twitter user -', () => {
     it('should authenticate successfully', () => {
       //expect 200 OK
     });
-
+    
     it('should call the appropriate method', () => {
       //expect tweetDM() to be called
     });
   });
-
+  
   describe('alerts sent to a bot twitter account -', () => {
     it('should authenticate successfully', () => {
       //expect 200 OK
     });
-
+    
     it('should call the appropriate method', () => {
       //expect tweetPublic() to be called
     });
